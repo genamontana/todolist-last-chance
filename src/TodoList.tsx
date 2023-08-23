149ad6ca1c072574dfcb737c3adfafccc7211a1e
genamontana <predator199@yandex.ru>
2023-08-23 14:53:31 +0300

    01 - Sprint 01, week 1 completed

---
+++ src/TodoList.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type TodoListType = {
+    title: string
+    tasks: TasksType[]
+}
+
+export function TodoList(props: TodoListType) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button></button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox"
+                           checked={props.tasks[0].isDone}/>
+                    <span>{props.tasks[0].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox"
+                           checked={props.tasks[1].isDone}/>
+                    <span>{props.tasks[1].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox"
+                           checked={props.tasks[2].isDone}/>
+                    <span>{props.tasks[2].title}</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
