7337c21d1250c6c9bce3294d1ff92786a20697f6
genamontana <predator199@yandex.ru>
2023-08-25 10:52:39 +0300

    09 - Sprint 03, week 1 in process

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -20,6 +20,7 @@ test('user reducer should increment only childrenCount', () => {
 
 test('user reducer should change name of user', () => {
     const startState = {name: 'Gena', age: 20, childrenCount: 2}
+
     const newName = 'Viktor'
     const endState = userReducer(startState, {type: 'CHANGE-NAME', newName: newName})
 
